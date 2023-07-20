e63c931f8d4875a5ea65ac2e6333e791d5b415d3
Rahman Are <rahmanare14@gmail.com>
2023-07-20 07:14:27 +0100

    quick tip fix

--- src/components/MainContent/StatisticReport.js
+++ src/components/MainContent/StatisticReport.js
@@ -1,5 +1,25 @@
 import Graph from "../../images/graph.png";
+import styled from 'styled-components';
+
 const StatisticReport = () => {
+    const DotStat = styled.p`
+        display: flex ;
+        align-items: center;
+        font-size: 14px;
+        &:before{
+            display: inline-block;
+            width: 8px;
+            height: 8px;
+            content: '';
+            border-radius: 50%;
+            margin-right: 8px;
+            
+            /* margin-left: -16px; */
+            
+            background-color: ${({ color }) => color};
+        }
+        /* color: ${({ color }) => color}; */
+    `;
     return (
         <div className="statistic-card">
             <div className='card-head'>
@@ -7,13 +27,13 @@ const StatisticReport = () => {
                     <h3>User Inflow Statistic</h3>
                     <p>Nov - July</p>
                 </div>
-                <div>
-                    <p className="card-head-title">Agents</p>
-                    <p className="card-head-number">Nov - July</p>
+                <div className="card-head-second">
+                    <DotStat color="#5f2eea" className="card-head-title">Agents</DotStat>
+                    <p className="card-head-number">473 273</p>
                 </div>
                 <div>
-                    <p className="card-head-title">Users</p>
-                    <p className="card-head-number">Nov - July</p>
+                    <DotStat color="#4cde97" className="card-head-title">Users</DotStat>
+                    <p className="card-head-number">782 396</p>
                 </div>
             </div>
             <div className="statistic-card_graph">
